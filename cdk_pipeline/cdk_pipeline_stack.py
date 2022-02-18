2387c5493839968851d47d1a8ba4cc13f0acdea5
Keenan Graham <ksgraham@stanford.edu>
2022-02-18 15:06:24 -0800

    Update

--- cdk_pipeline/cdk_pipeline_stack.py
+++ cdk_pipeline/cdk_pipeline_stack.py
@@ -1,19 +1,9 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack
+
 from constructs import Construct
+
 
 class CdkPipelineStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope, construct_id, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkPipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
