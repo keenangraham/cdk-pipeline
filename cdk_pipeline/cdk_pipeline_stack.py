3d4f546869e3dafc45056c66dd7155fec4287784
Keenan Graham <ksgraham@stanford.edu>
2022-02-23 10:34:52 -0800

    Rearrange

--- cdk_pipeline/cdk_pipeline_stack.py
+++ cdk_pipeline/cdk_pipeline_stack.py
@@ -9,27 +9,26 @@ class CdkPipelineStack(cdk.Stack):
 
     def __init__(self, scope, construct_id, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
-        self._make_pipeline()
-
-    def _make_pipeline(self):
-        self.pipeline = CodePipeline(
+        github = CodePipelineSource.connection(
+            'keenangraham/cdk-pipeline',
+            'main',
+            connection_arn=(
+                'arn:aws:codestar-connections:'
+                'us-east-1:618537831167:'
+                'connection/28ec4d05-97dd-4730-b41c-b3b698b2a485'
+            )
+        )
+        synth = ShellStep(
+            'Synth',
+            input=github,
+            commands=[
+                'npm install -g aws-cdk',
+                'python -m pip install -r requirements.txt',
+                'cdk synth',
+            ]
+        )
+        pipeline = CodePipeline(
             self,
             'Pipeline',
-            synth=ShellStep(
-                'Synth',
-                input=CodePipelineSource.connection(
-                    'keenangraham/cdk-pipeline',
-                    'main',
-                    connection_arn=(
-                        'arn:aws:codestar-connections:'
-                        'us-east-1:618537831167:'
-                        'connection/28ec4d05-97dd-4730-b41c-b3b698b2a485'
-                    )
-                ),
-                commands=[
-                    'npm install -g aws-cdk',
-                    'python -m pip install -r requirements.txt',
-                    'cdk synth',
-                ]
-            )
+            synth=synth
         )
