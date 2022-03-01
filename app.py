3667c7007f77ebf22bbf15e38419618c0f6cde61
Keenan Graham <ksgraham@stanford.edu>
2022-02-28 21:06:54 -0800

    Try to trigger from branch

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import aws_cdk as cdk
 
+from cdk_pipeline.config import config
+
 from cdk_pipeline.cdk_pipeline_stack import CdkPipelineStack
 from cdk_pipeline.notification_stack import NotificationStack
-from cdk_pipeline.config import config
 
 
 ENVIRONMENT = cdk.Environment(
