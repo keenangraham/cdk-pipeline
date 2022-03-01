625e1017974a335347b84cc5cd83460bf6dce9a4
Keenan Graham <ksgraham@stanford.edu>
2022-02-28 18:43:18 -0800

    Put in init

--- cdk_pipeline/continuous_integration_stack.py
+++ cdk_pipeline/continuous_integration_stack.py
@@ -14,34 +14,35 @@ class ContinuousIntegrationStack(cdk.Stack):
     def __init__(self, scope, construct_id, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-    github = Source.git_hub(
-        owner='keenangraham',
-        repo='cdk-pipeline',
-        webhook=True,     
-    )
+        github = Source.git_hub(
+            owner='keenangraham',
+            repo='cdk-pipeline',
+            webhook=True,
+        )
 
-    continuous_integration_project = Project(
-        'ContinuousIntegration',
-        source=github,
-        environment=BuildEnvironment(
-            build_image=LinuxBuildImage.STANDARD_5_0,
-            privileged=True,
-        ),
-        build_spec=BuildSpec.from_object(
-            {
-                'version': '0.2',
-                'phases': {
-                    'build': {
-                        'commands': [
-                            'echo CI',
-                            'echo $TEST',
-                        ]
+        continuous_integration_project = Project(
+            self,
+            'ContinuousIntegrationProject',
+            source=github,
+            environment=BuildEnvironment(
+                build_image=LinuxBuildImage.STANDARD_5_0,
+                privileged=True,
+            ),
+            build_spec=BuildSpec.from_object(
+                {
+                    'version': '0.2',
+                    'phases': {
+                        'build': {
+                            'commands': [
+                                'echo CI',
+                                'echo $TEST',
+                            ]
+                        }
                     }
                 }
-            }
-        ),
-        environment_variables={
-            'TEST': 'VALUE',
-        },
-        badge=True,
-    )
+            ),
+            environment_variables={
+                'TEST': 'VALUE',
+            },
+            badge=True,
+        )
