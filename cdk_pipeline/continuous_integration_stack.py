c2b504d97684976af1f59dfbf5f82a30f68bcc0e
Keenan Graham <ksgraham@stanford.edu>
2022-03-04 16:12:19 -0800

    Give public read permission to logs

--- cdk_pipeline/continuous_integration_stack.py
+++ cdk_pipeline/continuous_integration_stack.py
@@ -10,6 +10,10 @@ from aws_cdk.aws_codebuild import Project
 from aws_cdk.aws_codebuild import ReportGroup
 from aws_cdk.aws_codebuild import Source
 
+from aws_cdk.aws_iam import PolicyStatement
+from aws_cdk.aws_iam import Role
+from aws_cdk.aws_iam import ServicePrincipal
+
 from cdk_pipeline.naming import prepend_project_name
 
 
@@ -75,5 +79,33 @@ class ContinuousIntegrationStack(cdk.Stack):
             },
             badge=True,
         )
+
+        resource_access_role = Role(
+            self,
+            'ResourceAccessRole',
+            assumed_by=ServicePrincipal(
+                'codebuild.amazonaws.com'
+            )
+        )
+        project_name = continuous_integration_project.project_name
+        log_group_arn = cdk.Stack.of(self).format_arn(
+            service='logs',
+            resource='log-group',
+            arn_format=cdk.ArnFormat.COLON_RESOURCE_NAME,
+            resource_name=f'/aws/codebuild/{project_name}',
+        )
+        log_group_star_arn = f'{log_group_arn}:*'
+        public_read_policy = PolicyStatement(
+            resources=[
+                log_group_star_arn,
+            ],
+            actions=[
+                'logs:GetLogEvents'
+            ],
+        )
+        resource_access_role.add_to_principal_policy(
+            public_read_policy
+        )
         cfn_project = continuous_integration_project.node.default_child
         cfn_project.visibility = 'PUBLIC_READ'
+        cfn_project.resource_access_role = resource_access_role.role_arn
